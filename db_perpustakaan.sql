c459b6814d40819e932207b0bc18000f1da18c2c
pribbadi <pribadi.teguh90@gmail.com>
2015-08-28 06:18:46 +0700

    tes commit
    menambahkan database

---
+++ db_perpustakaan.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 16, 2014 at 06:29 
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+DROP TABLE IF EXISTS `anggota`;
+CREATE TABLE IF NOT EXISTS `anggota` (
+  `nis` varchar(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jk` varchar(2) DEFAULT NULL,
+  `ttl` date DEFAULT NULL,
+  `kelas` varchar(10) DEFAULT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`nis`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+DROP TABLE IF EXISTS `buku`;
+CREATE TABLE IF NOT EXISTS `buku` (
+  `kode_buku` varchar(5) NOT NULL,
+  `judul` varchar(100) DEFAULT NULL,
+  `pengarang` varchar(50) DEFAULT NULL,
+  `klasifikasi` varchar(25) DEFAULT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`kode_buku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+DROP TABLE IF EXISTS `pengembalian`;
+CREATE TABLE IF NOT EXISTS `pengembalian` (
+  `id_transaksi` varchar(12) DEFAULT NULL,
+  `tgl_pengembalian` date DEFAULT NULL,
+  `denda` varchar(2) DEFAULT NULL,
+  `nominal` double DEFAULT NULL,
+  `id_petugas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+DROP TABLE IF EXISTS `petugas`;
+CREATE TABLE IF NOT EXISTS `petugas` (
+  `id_petugas` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) DEFAULT NULL,
+  `password` text,
+  PRIMARY KEY (`id_petugas`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `user`, `password`) VALUES
+(2, 'jamal', '21232f297a57a5a743894a0e4a801fc3'),
+(3, 'admin', '0192023a7bbd73250516f069df18b500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmp`
+--
+
+DROP TABLE IF EXISTS `tmp`;
+CREATE TABLE IF NOT EXISTS `tmp` (
+  `kode_buku` varchar(5) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `pengarang` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+DROP TABLE IF EXISTS `transaksi`;
+CREATE TABLE IF NOT EXISTS `transaksi` (
+  `id_transaksi` varchar(12) DEFAULT NULL,
+  `nis` varchar(10) DEFAULT NULL,
+  `kode_buku` varchar(5) DEFAULT NULL,
+  `tanggal_pinjam` date DEFAULT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `id_petugas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
